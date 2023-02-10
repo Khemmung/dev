70559e3ab315e7d3dee0e4207850177732647f6c
Khemmung <124992244+Khemmung@users.noreply.github.com>
2023-02-10 15:06:08 +0700

    o

--- StudentID.py
+++ StudentID.py
@@ -1,7 +1,12 @@
 def print_ID(student_ID):
     print("Student ID : ",student_ID)
+def calculate_sum(array):
+    return array.sum()
+
 if __name__ == "__main__":
     print_ID("6513354")
+    calculate_sum(np.array({1,2,3,4,5,6}))
 #YEAH010203
 #Ohohohoh123
 #21
+#84
